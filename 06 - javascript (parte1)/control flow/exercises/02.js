c7e55ff46a6d45b566a50517559641f01ae94d4e
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-10-21 14:20:30 -0300

    agrega ejercicio 2

---
+++ 06 - javascript (parte1)/control flow/exercises/02.js
@@ -0,0 +1,9 @@
+//                      EXERCISE -- LANDSCAPE OF PORTRAIT
+// LANDSCAPE = WIDTH > HEIGHT
+
+function isLandscape(width, height) {
+    return (width > height); // ? true : false
+    //esto hace lo mismo y limpia el codigo
+}
+
+console.log(isLandscape(25,1));
