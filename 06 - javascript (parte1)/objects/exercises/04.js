700aa06d7361ee15d94049d1e1b9a002e4534553
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-10-22 12:26:31 -0300

    agrega ejercicio 4

---
+++ 06 - javascript (parte1)/objects/exercises/04.js
@@ -0,0 +1,12 @@
+//                      EXERCISE -- BLOG POST OBJECT
+const post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 10,
+    comments: [   //deberia ser un array porque pueden haber varios comentarios
+        { author: 'd', body: 'e'},
+        { author: 'f', body: 'g'}
+    ],
+    isLive: true
+}
