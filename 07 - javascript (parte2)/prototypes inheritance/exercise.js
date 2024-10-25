b7671988b397f0d98c4a97eafcf45294e8889c37
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-10-25 10:41:44 -0300

    agrega ejercicio completo de prototypes inheritance

--- 07 - javascript (parte2)/prototypes inheritance/exercise.js
+++ 07 - javascript (parte2)/prototypes inheritance/exercise.js
@@ -1,33 +1,68 @@
-function HtmlSelectElement(array = []) {
-    let items = 0;
-
-    Object.defineProperty(this, 'items', {
-        get: function() {
-            return items;
-        }
-    });
-
-    this.addItem = function(item) {
-        if (typeof item === 'string')
-            item = Number(item);
-        
-        if (!item || typeof item !== 'number') 
-            throw new Error('Invalid item')
-        
-        array.push(item);
-
-        array.reduce((accumulator, current) => {
-            accumulator+=current;
-            return items = accumulator;
-        }, 0);
-        
-    }
-}
-
 function HtmlElement() {
-    this.click = function() { console.log('click') };
+  this.click = function () {
+    console.log("clicked");
+  };
 }
 
-HtmlElement.prototype.focus = function() { console.log('focus') };
+HtmlElement.prototype.focus = function () {
+  console.log("focued");
+};
+
+function HtmlSelectElement(items = []) {
+  this.items = items;
+
+  this.addItem = function (item) {
+    if (typeof item === "string") item = Number(item);
+
+    if (!item || typeof item !== "number") throw new Error("Invalid item");
+
+    this.items.push(item);
+  };
+
+  this.removeItem = function (item) {
+    if (typeof item === "string") item = Number(item);
+
+    if (!item || typeof item !== "number") throw new Error("Invalid item");
+
+    this.items.slice(this.items.indexOf(item), 1);
+  };
+
+  this.render = function () {
+    return `
+    <select>${this.items
+      .map(
+        (item) => `
+        <option>${item}</option>`
+      )
+      .join(" ")}
+    </select>`;
+    //es necesario el join para que junte todo con un espcio y no una coma
+  };
+}
+
+//esta implementacion no hubiese funcionado
+// HtmlSelectElement.prototype = Object.create(HtmlElement.prototype);
+//porque object.create crearia un nuevo objeto y setearia su prototipo
+//al prototipo de htmlelement
+//este prototipo, llamemoslo baseHtmlElement tiene solo un metodo (focus)
+
+//por lo que se debe hacer lo siguiente
+HtmlSelectElement.prototype = new HtmlElement();
+//esto crea un nuevo objeto de htmlelement que su prototipo es
+//basehtmlelement, el cual tiene el metodo focus
+//y el objeto creado tiene el metodo click
+//por lo que creamos una instancia de htmlElement
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
 const e = new HtmlSelectElement();
+
+function HtmlImageElement(src) {
+  this.src = src;
+
+  this.render = function () {
+    return `<img src="${this.src}" />`;
+  };
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
