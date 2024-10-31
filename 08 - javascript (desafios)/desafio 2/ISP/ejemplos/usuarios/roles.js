8fc3103c7fd98e39a8e37c7b10635087ef325e16
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-10-31 12:09:23 -0300

    elimina codigo repetido

--- 08 - javascript (desafios)/desafio 2/ISP/ejemplos/usuarios/roles.js
+++ 08 - javascript (desafios)/desafio 2/ISP/ejemplos/usuarios/roles.js
@@ -5,18 +5,10 @@ class Administrador extends AgregarUsuarioMixin(Usuarios) {
   constructor(nombre, contraseña) {
     super(nombre, contraseña);
   }
-
-  agregarUsuario() {
-    return console.log("agregaste a un usuario!");
-  }
 }
 
 class Dueño extends AgregarUsuarioMixin(Usuarios) {
   constructor(nombre, contraseña) {
     super(nombre, contraseña);
-  }
-
-  agregarUsuario() {
-    return console.log("agregaste a un usuario!");
   }
 }
