19b79ec8bf8d406af8f2b0364dfeec5536f4693a
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-11-21 13:38:42 -0300

    se instala prettier y formatea los archivos

--- 09 - javascript unit testing/tests/core.test.js
+++ 09 - javascript unit testing/tests/core.test.js
@@ -7,7 +7,7 @@ import {
   isValidUsername,
   Stack,
   validateUserInput,
-} from "../src/core";
+} from '../src/core';
 import {
   it,
   expect,
@@ -16,7 +16,7 @@ import {
   beforeAll,
   afterAll,
   afterEach,
-} from "vitest";
+} from 'vitest';
 // si ponemos iv el vitest snippet nos importa automaticamente todo
 
 // ------------------------WRITING GOOD ASSERTIONS-----------------------
@@ -65,8 +65,8 @@ describe("test suite", () => {
 */
 
 // Exercise
-describe("getCoupons", () => {
-  it("should return an array of coupons", () => {
+describe('getCoupons', () => {
+  it('should return an array of coupons', () => {
     const coupons = getCoupons();
 
     // es mejor incluir la primera por si te sale un error, es mas
@@ -76,22 +76,22 @@ describe("getCoupons", () => {
     expect(coupons.length).toBeGreaterThan(0);
   });
 
-  it("should return an array with valid coupon codes", () => {
+  it('should return an array with valid coupon codes', () => {
     const coupons = getCoupons();
 
     coupons.forEach((coupon) => {
-      expect(coupon).toHaveProperty("code");
-      expect(typeof coupon.code).toBe("string");
+      expect(coupon).toHaveProperty('code');
+      expect(typeof coupon.code).toBe('string');
       expect(coupon.code).toBeTruthy(); // strings vacios no son truthy
     });
   });
 
-  it("should return an array with valid discounts", () => {
+  it('should return an array with valid discounts', () => {
     const coupons = getCoupons();
 
     coupons.forEach((coupon) => {
-      expect(coupon).toHaveProperty("discount");
-      expect(typeof coupon.discount).toBe("number");
+      expect(coupon).toHaveProperty('discount');
+      expect(typeof coupon.discount).toBe('number');
       expect(coupon.discount).toBeGreaterThan(0);
       expect(coupon.discount).toBeLessThan(1);
     });
@@ -101,72 +101,72 @@ describe("getCoupons", () => {
 });
 
 // ----------------POSITIVE AND NEGATIVE TESTING--------------------------
-describe("calculateDiscount", () => {
+describe('calculateDiscount', () => {
   //positive
-  it("should return discounted price if given valid code", () => {
-    expect(calculateDiscount(10, "SAVE10")).toBe(9);
+  it('should return discounted price if given valid code', () => {
+    expect(calculateDiscount(10, 'SAVE10')).toBe(9);
     // esta assertion la agregamos despues cuando vimos por coverage que no estaba siendo testeada
-    expect(calculateDiscount(10, "SAVE20")).toBe(8);
+    expect(calculateDiscount(10, 'SAVE20')).toBe(8);
   });
 
   //negative
-  it("should handle non-numeric price", () => {
+  it('should handle non-numeric price', () => {
     // con typescript no va a ser necesario
-    expect(calculateDiscount("10", "SAVE10")).toMatch(/invalid/i);
+    expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
   });
 
-  it("should handle negative price", () => {
-    expect(calculateDiscount(-10, "SAVE10")).toMatch(/invalid/i);
+  it('should handle negative price', () => {
+    expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
   });
 
-  it("should handle non-string discount code", () => {
+  it('should handle non-string discount code', () => {
     expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
   });
 
-  it("should handle invalid discount code", () => {
-    expect(calculateDiscount(10, "INVALID")).toBe(10);
+  it('should handle invalid discount code', () => {
+    expect(calculateDiscount(10, 'INVALID')).toBe(10);
   });
 });
 
 // Exercise
-describe("validateUserInput", () => {
-  it("should return success message if given valid username and age", () => {
-    expect(validateUserInput("micaela", 21)).toMatch(/success/i);
+describe('validateUserInput', () => {
+  it('should return success message if given valid username and age', () => {
+    expect(validateUserInput('micaela', 21)).toMatch(/success/i);
   });
 
-  it("should return error message if given a non-string username", () => {
+  it('should return error message if given a non-string username', () => {
     expect(validateUserInput(10, 20)).toMatch(/invalid/i);
   });
 
-  it("should return error message if given a username of length shorter than 3", () => {
-    expect(validateUserInput("a", 20)).toMatch(/invalid/i);
+  it('should return error message if given a username of length shorter than 3', () => {
+    expect(validateUserInput('a', 20)).toMatch(/invalid/i);
   });
 
   //esta la agregó mosh
-  it("should return error message if username is longer than 255 characters", () => {
-    expect(validateUserInput("a".repeat(256), 20)).toMatch(/invalid/i);
+  it('should return error message if username is longer than 255 characters', () => {
+    expect(validateUserInput('a'.repeat(256), 20)).toMatch(/invalid/i);
   });
 
-  it("should return error message if given a non-number age", () => {
-    expect(validateUserInput("micaela", "20")).toMatch(/invalid/i);
+  it('should return error message if given a non-number age', () => {
+    expect(validateUserInput('micaela', '20')).toMatch(/invalid/i);
   });
 
-  it("should return error message if given an age under 18", () => {
-    expect(validateUserInput("micaela", 15)).toMatch(/invalid/i);
+  it('should return error message if given an age under 18', () => {
+    expect(validateUserInput('micaela', 15)).toMatch(/invalid/i);
   });
 
   // estas dos la agregó mosh
-  it("should return an error if age is greater than 100", () => {
-    expect(validateUserInput("micaela", 101)).toMatch(/invalid/i);
+  it('should return an error if age is greater than 100', () => {
+    expect(validateUserInput('micaela', 101)).toMatch(/invalid/i);
   });
-  it("should return an error if both username and age are invalid", () => {
-    expect(validateUserInput("", 0)).toMatch(/invalid username/i);
-    expect(validateUserInput("", 0)).toMatch(/invalid age/i);
+  it('should return an error if both username and age are invalid', () => {
+    expect(validateUserInput('', 0)).toMatch(/invalid username/i);
+    expect(validateUserInput('', 0)).toMatch(/invalid age/i);
   });
 });
 
 //----------------------BOUNDARY TESTING-----------------------------------
-describe("isPriceInRange", () => {
+describe('isPriceInRange', () => {
   // it("should return false when the price is outside the range", () => {
   //   expect(isPriceInRange(-10, 0, 100)).toBe(false);
   //   expect(isPriceInRange(200, 0, 100)).toBe(false);
@@ -183,40 +183,40 @@ describe("isPriceInRange", () => {
 
   // Exercise Parameterized Tests
   it.each([
-    { scenario: "price < min", price: -10, result: false },
-    { scenario: "price = min", price: 0, result: true },
-    { scenario: "price between min and max", price: 50, result: true },
-    { scenario: "price = max", price: 100, result: true },
-    { scenario: "price > max", price: 200, result: false },
-  ])("should return $result when $scenario", ({ price, result }) => {
+    { scenario: 'price < min', price: -10, result: false },
+    { scenario: 'price = min', price: 0, result: true },
+    { scenario: 'price between min and max', price: 50, result: true },
+    { scenario: 'price = max', price: 100, result: true },
+    { scenario: 'price > max', price: 200, result: false },
+  ])('should return $result when $scenario', ({ price, result }) => {
     expect(isPriceInRange(price, 0, 100)).toBe(result);
   });
 });
 
 // Exercise
-describe("isValidUsername", () => {
+describe('isValidUsername', () => {
   const minLength = 5;
   const maxLength = 15;
 
-  it("should return false if username is too short", () => {
-    expect(isValidUsername("a".repeat(minLength - 1))).toBe(false);
+  it('should return false if username is too short', () => {
+    expect(isValidUsername('a'.repeat(minLength - 1))).toBe(false);
   });
 
-  it("should return false if username is too long", () => {
-    expect(isValidUsername("a".repeat(maxLength + 1))).toBe(false);
+  it('should return false if username is too long', () => {
+    expect(isValidUsername('a'.repeat(maxLength + 1))).toBe(false);
   });
 
-  it("should return true if username is at the min or max length", () => {
-    expect(isValidUsername("a".repeat(maxLength))).toBe(true);
-    expect(isValidUsername("a".repeat(minLength))).toBe(true);
+  it('should return true if username is at the min or max length', () => {
+    expect(isValidUsername('a'.repeat(maxLength))).toBe(true);
+    expect(isValidUsername('a'.repeat(minLength))).toBe(true);
   });
 
-  it("should return true if username is within the length constraint", () => {
-    expect(isValidUsername("a".repeat(maxLength - 1))).toBe(true);
-    expect(isValidUsername("a".repeat(minLength + 1))).toBe(true);
+  it('should return true if username is within the length constraint', () => {
+    expect(isValidUsername('a'.repeat(maxLength - 1))).toBe(true);
+    expect(isValidUsername('a'.repeat(minLength + 1))).toBe(true);
   });
 
-  it("should return false for invalid input types", () => {
+  it('should return false for invalid input types', () => {
     expect(isValidUsername(null)).toBe(false);
     expect(isValidUsername(undefined)).toBe(false);
     expect(isValidUsername(1)).toBe(false);
@@ -224,20 +224,20 @@ describe("isValidUsername", () => {
 });
 
 // Exercise
-describe("canDrive", () => {
-  it("should return error for invalid country code", () => {
-    expect(canDrive(20, "FR")).toMatch(/invalid/i);
+describe('canDrive', () => {
+  it('should return error for invalid country code', () => {
+    expect(canDrive(20, 'FR')).toMatch(/invalid/i);
   });
 
   // Parameterized Tests
   it.each([
-    { age: 15, country: "US", result: false },
-    { age: 16, country: "US", result: true },
-    { age: 17, country: "US", result: true },
-    { age: 16, country: "UK", result: false },
-    { age: 17, country: "UK", result: true },
-    { age: 18, country: "UK", result: true },
-  ])("should return $result for $age, $country", ({ age, country, result }) => {
+    { age: 15, country: 'US', result: false },
+    { age: 16, country: 'US', result: true },
+    { age: 17, country: 'US', result: true },
+    { age: 16, country: 'UK', result: false },
+    { age: 17, country: 'UK', result: true },
+    { age: 18, country: 'UK', result: true },
+  ])('should return $result for $age, $country', ({ age, country, result }) => {
     expect(canDrive(age, country)).toBe(result);
   }); // esto retorna una funcion de la misma forma que llamamos a la it function,
   // le pasamos el nombre del test y la test function
@@ -269,7 +269,7 @@ describe("canDrive", () => {
 });
 
 // -----------------------TESTING ASYNCHRONOUS CODE--------------------
-describe("fetchData", () => {
+describe('fetchData', () => {
   // it("should return a promise that will resolve to an array of numbers", () => {
   //   fetchData().then((result) => {
   //     expect(Array.isArray(result)).toBe(true);
@@ -285,12 +285,12 @@ describe("fetchData", () => {
   // });
 
   // para simular promesa rechazada
-  it("should return a promise that will resolve to an array of numbers", async () => {
+  it('should return a promise that will resolve to an array of numbers', async () => {
     try {
       const result = await fetchData();
     } catch (error) {
       // el objeto con el resultado de la promesa es el error
-      expect(error).toHaveProperty("reason");
+      expect(error).toHaveProperty('reason');
       expect(error.reason).toMatch(/fail/i);
     }
   });
@@ -301,44 +301,44 @@ describe("fetchData", () => {
 // por lo que importo lo siguiente
 // beforeEach, beforeAll, afterAll, afterEach de Vitest
 
-describe("test suite", () => {
+describe('test suite', () => {
   beforeAll(() => {
-    console.log("beforeAll called");
+    console.log('beforeAll called');
   });
 
   // la funcion callback se ejecuta antes de cada prueba
   beforeEach(() => {
-    console.log("beforeEach called");
+    console.log('beforeEach called');
   });
 
   afterEach(() => {
-    console.log("afterEach called");
+    console.log('afterEach called');
   });
 
   afterAll(() => {
-    console.log("afterAll called");
+    console.log('afterAll called');
   });
 
-  it("test case 1", () => {});
+  it('test case 1', () => {});
 
-  it("test case 2", () => {});
+  it('test case 2', () => {});
 });
 
 // Exercise
-describe("Stack", () => {
+describe('Stack', () => {
   let stack;
 
   beforeEach(() => {
     stack = new Stack();
   });
 
-  it("push should add an item to the stack", () => {
+  it('push should add an item to the stack', () => {
     stack.push(1);
 
     expect(stack.size()).toBe(1);
   });
 
-  it("pop should remove and return the top item from the stack", () => {
+  it('pop should remove and return the top item from the stack', () => {
     stack.push(1);
     stack.push(2);
 
@@ -349,11 +349,11 @@ describe("Stack", () => {
   });
 
   // PARA MANEJAR ERRORES
-  it("pop should throw an error if stack is empty", () => {
+  it('pop should throw an error if stack is empty', () => {
     expect(() => stack.pop()).toThrow(/empty/i);
   });
 
-  it("peek should return the top item from the stack without removing it", () => {
+  it('peek should return the top item from the stack without removing it', () => {
     stack.push(1);
     stack.push(2);
 
@@ -363,28 +363,28 @@ describe("Stack", () => {
     expect(stack.size()).toBe(2);
   });
 
-  it("peek should throw an error if stack is empty", () => {
+  it('peek should throw an error if stack is empty', () => {
     expect(() => stack.peek()).toThrow(/empty/i);
   });
 
-  it("isEmpty should return true if stack is empty", () => {
+  it('isEmpty should return true if stack is empty', () => {
     expect(stack.isEmpty()).toBe(true);
   });
 
-  it("isEmpty should return false if stack is not empty", () => {
+  it('isEmpty should return false if stack is not empty', () => {
     stack.push(1);
 
     expect(stack.isEmpty()).toBe(false);
   });
 
-  it("size should return the number of items in the stack", () => {
+  it('size should return the number of items in the stack', () => {
     stack.push(1);
     stack.push(2);
 
     expect(stack.size()).toBe(2);
   });
 
-  it("clear should remove all items from the stack", () => {
+  it('clear should remove all items from the stack', () => {
     stack.push(1);
     stack.push(2);
 
--- 09 - javascript unit testing/tests/mocking.test.js
+++ 09 - javascript unit testing/tests/mocking.test.js
@@ -1,8 +1,8 @@
-import { trackPageView } from "../src/libs/analytics";
-import { getExchangeRate } from "../src/libs/currency";
-import { sendEmail } from "../src/libs/email";
-import { charge } from "../src/libs/payment";
-import { getShippingQuote } from "../src/libs/shipping";
+import { trackPageView } from '../src/libs/analytics';
+import { getExchangeRate } from '../src/libs/currency';
+import { sendEmail } from '../src/libs/email';
+import { charge } from '../src/libs/payment';
+import { getShippingQuote } from '../src/libs/shipping';
 import {
   getDiscount,
   getPriceInCurrency,
@@ -12,15 +12,15 @@ import {
   renderPage,
   signUp,
   submitOrder,
-} from "../src/mocking";
-import { vi, it, expect, describe, beforeEach } from "vitest";
+} from '../src/mocking';
+import { vi, it, expect, describe, beforeEach } from 'vitest';
 // SYPING ON FUNCTIONS--------------------------
-import security from "../src/libs/security";
+import security from '../src/libs/security';
 // ---------------------------------------------
 
 // TODO: INFO IMPORTANTE------------------------------------
 // la propiedad mock es global, acumula informacion entre
-// distintos casos de prueba. Como buena practica se debe 
+// distintos casos de prueba. Como buena practica se debe
 // siempre limpiar nuestras mockfunctions antes o despues
 // de cada caso de prueba
 // mockClear() --> limpia toda la informacion de tods las llamadas
@@ -35,13 +35,12 @@ import security from "../src/libs/security";
 // EJEMPLO EN VITEST.CONFIG.JS
 // ---------------------------------------------------------
 
-
 // vi tiene un metodo para crear mock functions
 
-vi.mock("../src/libs/currency");
-vi.mock("../src/libs/shipping");
-vi.mock("../src/libs/analytics");
-vi.mock("../src/libs/payment");
+vi.mock('../src/libs/currency');
+vi.mock('../src/libs/shipping');
+vi.mock('../src/libs/analytics');
+vi.mock('../src/libs/payment');
 
 //-------------------PARTIAL MOCKING-----------------------------------
 // hay situaciones donde queremos mantener alguna de las funciones en el modulo
@@ -51,7 +50,7 @@ vi.mock("../src/libs/payment");
 // cuando testeamos el código no vamos a querer enviar emails
 // pero para isValidEmail lo unico que hace es chequear que esté bien
 // no queremos reemplazarla por una mock function
-vi.mock("../src/libs/email", async (importOriginal) => {
+vi.mock('../src/libs/email', async (importOriginal) => {
   const originalModule = await importOriginal(); //devuelve una promesa
   return {
     ...originalModule, //copiamos todas las propiedades del modulo
@@ -60,8 +59,8 @@ vi.mock("../src/libs/email", async (importOriginal) => {
 });
 //---------------------------------------------------------------------
 
-describe("test suite", () => {
-  it("test case", () => {
+describe('test suite', () => {
+  it('test case', () => {
     const greet = vi.fn(); // retorna una mock function. por default no tiene
     // ningun comportamiento, retorna undefined
 
@@ -75,30 +74,30 @@ describe("test suite", () => {
     // greet().then(result => console.log(result));
 
     //para pasarle logica
-    greet.mockImplementation((name) => "Hello" + name);
-    const result = greet("Mosh");
+    greet.mockImplementation((name) => 'Hello' + name);
+    const result = greet('Mosh');
     console.log(result);
 
     //assertions de las mock functions
     //que haya sido llamada la funcion
     expect(greet).toHaveBeenCalled();
     //que haya sido llamada con un parametro en especifico
-    expect(greet).toHaveBeenCalledWith("Mosh");
+    expect(greet).toHaveBeenCalledWith('Mosh');
     //que haya sido llamada solo una vez
     expect(greet).toHaveBeenCalledOnce();
   });
 });
 
 // Exercise
-describe("test suite 2", () => {
-  it("test case 2", () => {
+describe('test suite 2', () => {
+  it('test case 2', () => {
     const sendText = vi.fn();
-    sendText.mockReturnValue("ok");
+    sendText.mockReturnValue('ok');
 
-    const result = sendText("message");
+    const result = sendText('message');
 
-    expect(sendText).toHaveBeenCalledWith("message");
-    expect(result).toBe("ok");
+    expect(sendText).toHaveBeenCalledWith('message');
+    expect(result).toBe('ok');
   });
 });
 
@@ -109,32 +108,32 @@ describe("test suite 2", () => {
 //vi.mock("../src/libs/currency");
 //cuando lo llamamos devuelve todas las funciones que se exportan del módulo
 //esa linea de codigo es hoisteada al principio de todo, antes que las importaciones
-describe("getPriceInCurrency", () => {
-  it("should return price in target currency", () => {
+describe('getPriceInCurrency', () => {
+  it('should return price in target currency', () => {
     vi.mocked(getExchangeRate).mockReturnValue(1.5);
 
-    const price = getPriceInCurrency(10, "AUD");
+    const price = getPriceInCurrency(10, 'AUD');
 
     expect(price).toBe(15);
   });
 });
 
 // Exercise
-describe("getShippingInfo", () => {
-  it("should return shipping unavailable if quote cannot be fetched", () => {
+describe('getShippingInfo', () => {
+  it('should return shipping unavailable if quote cannot be fetched', () => {
     vi.mocked(getShippingQuote).mockReturnValue(null);
 
-    const result = getShippingInfo("London");
+    const result = getShippingInfo('London');
 
     expect(result).toMatch(/unavailable/i);
   });
 
-  it("should return shipping info if quote can be fetched", () => {
+  it('should return shipping info if quote can be fetched', () => {
     vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
 
-    const result = getShippingInfo("London");
+    const result = getShippingInfo('London');
 
-    expect(result).toMatch("$10");
+    expect(result).toMatch('$10');
     expect(result).toMatch(/2 days/i);
     //otra forma, mismo resultado
     expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i);
@@ -142,74 +141,74 @@ describe("getShippingInfo", () => {
 });
 
 //--------------------TESTING INTERACTIONS-----------------------------
-describe("renderPage", () => {
-  it("should return correct content", async () => {
+describe('renderPage', () => {
+  it('should return correct content', async () => {
     const result = await renderPage();
 
     expect(result).toMatch(/content/i);
   });
 
-  it("should call analytics", async () => {
+  it('should call analytics', async () => {
     await renderPage();
 
-    expect(trackPageView).toHaveBeenCalledWith("/home");
+    expect(trackPageView).toHaveBeenCalledWith('/home');
   });
 });
 
 // Exercise
-describe("submitOrder", () => {
+describe('submitOrder', () => {
   const order = { totalAmount: 10 };
-  const creditCard = { creditCardNumber: "1234" };
+  const creditCard = { creditCardNumber: '1234' };
 
-  it("should charge the customer", async () => {
-    vi.mocked(charge).mockResolvedValue({ status: "success" });
+  it('should charge the customer', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'success' });
 
     await submitOrder(order, creditCard);
 
     expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
   });
 
-  it("should return success when payment is successful", async () => {
-    vi.mocked(charge).mockResolvedValue({ status: "success" });
+  it('should return success when payment is successful', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'success' });
 
     const result = await submitOrder(order, creditCard);
 
     expect(result).toEqual({ success: true });
   });
 
-  it("should not return success when payment is successful", async () => {
-    vi.mocked(charge).mockResolvedValue({ status: "failed" });
+  it('should not return success when payment is successful', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'failed' });
 
     const result = await submitOrder(order, creditCard);
 
-    expect(result).toEqual({ success: false, error: "payment_error" });
+    expect(result).toEqual({ success: false, error: 'payment_error' });
   });
 });
 
 //-------------------------PARTIAL MOCKING-------------------------------
-describe("signUp", () => {
-  const email = "name@domain.com";
+describe('signUp', () => {
+  const email = 'name@domain.com';
 
   // CLEARING MOCKS
   beforeEach(() => {
     vi.mocked(sendEmail).mockClear();
     // FORMA DE LIMPIAR TODOS LOS MOCKS
     vi.clearAllMocks();
-  })
+  });
 
-  it("should return false if email is not valid", async () => {
-    const result = await signUp("a");
+  it('should return false if email is not valid', async () => {
+    const result = await signUp('a');
 
     expect(result).toBe(false);
   });
 
-  it("should return true if email is valid", async () => {
+  it('should return true if email is valid', async () => {
     const result = await signUp(email);
 
     expect(result).toBe(true);
   });
 
-  it("should return the welcome email if email is valid", async () => {
+  it('should return the welcome email if email is valid', async () => {
     const result = await signUp(email);
 
     expect(sendEmail).toHaveBeenCalled();
@@ -232,54 +231,54 @@ describe("signUp", () => {
 // importamos primero el modulo (se ve arriba de todo)
 describe('login', () => {
   it('should email the one-time login code', async () => {
-    const email = "name@domain.com";
+    const email = 'name@domain.com';
     // es muy parecido a mock
-    const spy = vi.spyOn(security, "generateCode");
+    const spy = vi.spyOn(security, 'generateCode');
 
     await login(email);
 
     const securityCode = spy.mock.results[0].value.toString();
-    expect(sendEmail).toHaveBeenCalledWith(email, securityCode)
-  })
-})
+    expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
+  });
+});
 
 // -------------------------MOCKING DATES------------------------------
-// para isOnline, dependemos del tiempo actual. el new Date() va a 
+// para isOnline, dependemos del tiempo actual. el new Date() va a
 // cambiar siempre que ejecutemos la prueba
 // para eso debemos mockear la funcion date y simular distintos escenarios
 describe('isOnline', () => {
   it('should return false if current hour is outside opening hours', () => {
-    vi.setSystemTime("2024-01-01 07:59");
+    vi.setSystemTime('2024-01-01 07:59');
     expect(isOnline()).toBe(false);
 
-    vi.setSystemTime("2024-01-01 20:01");
+    vi.setSystemTime('2024-01-01 20:01');
     expect(isOnline()).toBe(false);
-  })
+  });
 
   it('should return true if current hour is within opening hours', () => {
-    vi.setSystemTime("2024-01-01 8:00");
+    vi.setSystemTime('2024-01-01 8:00');
     expect(isOnline()).toBe(true);
 
-    vi.setSystemTime("2024-01-01 19:59");
+    vi.setSystemTime('2024-01-01 19:59');
     expect(isOnline()).toBe(true);
-  })
-})
+  });
+});
 
 // Exercise
 describe('getDiscount', () => {
   it('should return .2 on Christmas day', () => {
-    vi.setSystemTime("2024-12-25 00:01")
-    expect(getDiscount()).toBe(.2);
+    vi.setSystemTime('2024-12-25 00:01');
+    expect(getDiscount()).toBe(0.2);
 
-    vi.setSystemTime("2024-12-25 23:59")
-    expect(getDiscount()).toBe(.2);
-  })
+    vi.setSystemTime('2024-12-25 23:59');
+    expect(getDiscount()).toBe(0.2);
+  });
 
   it('should return 0 on any other day', () => {
-    vi.setSystemTime("2024-12-24 00:01")
+    vi.setSystemTime('2024-12-24 00:01');
     expect(getDiscount()).toBe(0);
 
-    vi.setSystemTime("2024-12-26 00:01")
+    vi.setSystemTime('2024-12-26 00:01');
     expect(getDiscount()).toBe(0);
-  })
-})
+  });
+});
