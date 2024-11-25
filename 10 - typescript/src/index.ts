e7c505ddc815157aa4bc420c06d804a36e6feedb
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-11-25 11:29:45 -0300

    agrega teoria de tipos avanzados

--- 10 - typescript/src/index.ts
+++ 10 - typescript/src/index.ts
@@ -92,14 +92,176 @@ ejemplo(19);
 // Objetos
 // ya no se pueden crear propiedades o metodos de la forma
 // objeto.propiedad = "hola"
-let employee: {
-    readonly id: number, // para no poder modificar esto en un futuro
+// let employee: {
+//     readonly id: number, // para no poder modificar esto en un futuro
+//     name: string,
+//     retire: (date: Date) => void
+// } = { 
+//     id: 1, 
+//     name: "Mosh",
+//     retire: (date: Date) => {
+//         console.log(date)
+//     }
+// };
+// -----------------------------------------------------------------------
+
+
+// ADVANCED TYPES--------------------------------------------------------
+
+// Type aliases
+// podemos definir un tipo custom. esto es mas que nada porque el objeto
+// creado antes es muy dificil de leer.
+type Employee = {
+    readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = { 
+}
+
+let employee: Employee = {
     id: 1, 
     name: "Mosh",
     retire: (date: Date) => {
         console.log(date)
     }
+}
+
+
+// Union Types
+// para que un parametro/variable pueda tener mas de un tipo
+// function kgToLbs(weight: number | string): number {
+
+// }
+
+// Narrowing se usa para ser mas especificos con los tipos
+// y poder acceder a los metodos de ese tipo. de la funcion de
+// arriba, solo weight puede acceder a los metodos que comparten
+// los numeros y los strings
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else 
+        return parseInt(weight) * 2.2;
+}
+
+
+// Intersection Types
+// una variable puede ser dos tipos al mismo tiempo
+type Draggable = {
+    drag: () => void
 };
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
+
+
+// Literal Types
+// a veces queremos limitar los valores que le queremos asignar a una variable
+let login: "y" | "n" = "y";
+
+// otra forma
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+
+// Nullable Types
+// para poder pasar un valor null en una funcion sin que nos tire error
+// podemos usar el operador de union types
+function greet(name: string | null | undefined) {
+    if(name)
+        console.log(name.toUpperCase());
+    else
+        console.log("Hola!");
+}
+
+greet(null);
+greet(undefined);
+
+
+// Optional Chaining
+// type Customer = {
+//     birthday: Date
+// };
+
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(0);
+// optional property access operator
+console.log(customer?.birthday); // solo se ejecuta si existe la propiedad
+
+type Customer = {
+    birthday?: Date
+};
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// util para los arrays
+// customers?.[0]
+
+// Optional call (funciones)
+let log: any = null;
+log?.("a"); //solo se ejecuta si log hace referencia a una funcion
+
+
+// Nullish Coaelscing Operator
+// chequea que el valor sea null o undefined
+// sirve para casos donde queres cheqeuar los falsy pero hay un valor
+// falsy valid, como el 0
+let speed: number | null = null;
+let ride = {
+    speed: speed ?? 30
+}
+
+
+// Type Assertions
+// a veces sabemos mas del tipo de un objeto que ts
+let $phone = document.getElementById("phone") as HTMLInputElement;
+$phone.value;
+// esto no convierne a phone en un inputelement, es solo para darle info
+// al compilador. en caso de ser otra cosa en vez de inputelement
+// el programa se va a romper cuando querramos ejecutar
+// phone.value
+
+// hay otra sintax para lograr lo mismo
+// let $phone = <HTMLInputElement> document.getElementById("phone");
+
+
+// The unknown type 
+// si queremos tener valores de tipo any pero no queremos que el 
+// programa crashee si es que accede a metodos que no existen
+// podemos utilizar el tipo unknown y narrowing
+function render(document: unknown) {
+    if (typeof document === "string")
+        document.toUpperCase();
+}
+
+
+// The never type
+// representa valores que nunca ocurren
+function processEvents(): never { // para indicar que nunca retorna
+    while (true){
+        //dkasdklaklsd
+    }
+}
+// processEvents();
+
+// console.log("hola"); tira error porque nunca se va a ejecutar
+// luego del loop infinito
+// si no ponemos never, la funcion aparece como void y no aparece el
+// error de que no se puede ejecutar el codigo de abajo
+
+function reject(): never {
+    throw new Error("error");
+}
+
+// reject();
+// console.log("hola") tira error
