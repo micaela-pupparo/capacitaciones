5240ab9b53d54f9610e0d80ce2bcb11fce965cdc
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-11-29 15:52:19 -0300

    crea primer componente Counter

---
+++ 11 - react16/my-react-app/src/components/counter.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import * as React from 'react';
+// import { Component } from 'react';
+
+
+interface Props {
+    
+}
+ 
+interface State {}
+ 
+class Counter extends React.Component<Props, State> {
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Counter;
--- 11 - react16/my-react-app/src/main.tsx
+++ 11 - react16/my-react-app/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from './components/counter.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Counter />
   </StrictMode>,
 )
