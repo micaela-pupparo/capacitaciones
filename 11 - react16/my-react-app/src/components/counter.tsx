9f85ea54d2733a4c2f3c4c2f5db5d2fd75a5609a
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-11-29 16:35:07 -0300

    modifica el div por un react fragment

--- 11 - react16/my-react-app/src/components/counter.tsx
+++ 11 - react16/my-react-app/src/components/counter.tsx
@@ -1,18 +1,20 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-import * as React from 'react';
+import * as React from "react";
 // import { Component } from 'react';
 
+interface Props {}
 
-interface Props {
-    
-}
- 
 interface State {}
- 
+
 class Counter extends React.Component<Props, State> {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Counter;
