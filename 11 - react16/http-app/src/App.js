d99d1dfaff713108419b7ef06b822dadfdb94391
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-06 15:22:51 -0300

    maneja errores esperados e inesperados

--- 11 - react16/http-app/src/App.js
+++ 11 - react16/http-app/src/App.js
@@ -42,7 +42,13 @@ class App extends Component {
     try {
       await axios.delete(apiEndpoint + "/" + post.id);
     } catch (ex) {
-      alert("something went wrong while deleting a post!");
+      if (ex.response && ex.response.status === 404)
+        alert("This post has already been deleted.");
+      else {
+        console.log("Logging the error", ex);
+        alert("An unexpected error occurred.");
+      }
+
       this.setState({ posts: originalPosts });
     }
   };
