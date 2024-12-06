a6724458bbb281d87b073712205912965dd2d00b
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-06 09:31:13 -0300

    agrega teoria de refs

--- 11 - react16/my-react-app/src/components/LoginForm.jsx
+++ 11 - react16/my-react-app/src/components/LoginForm.jsx
@@ -3,9 +3,16 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   // para poder acceder a un elemento del dom sin document.queryselector, creamos una referencia con react
   username = React.createRef();
+  // se deberia minimizar el uso de refs y usarlos solo cuando realmente se necesitan. para este caso los estamos utilizando para acceder al focus del input field.
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // } esto es totalmente al pedo porque podemos usar el atributo autofocus
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    // luego con esa referencia podemos acceder al elemento con current z de ahi sacamos su valor
+    const username = this.username.current.value;
     console.log("submitted");
   };
 
@@ -16,7 +23,12 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
