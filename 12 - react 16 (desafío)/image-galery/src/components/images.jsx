5045f70dac3374408f099c0c9bed7952cd55706e
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-12 15:18:05 -0300

    consume api de imagenes

--- 12 - react 16 (desafío)/image-galery/src/components/images.jsx
+++ 12 - react 16 (desafío)/image-galery/src/components/images.jsx
@@ -1,9 +1,27 @@
 import { Component } from "react";
+import http from "../services/httpService";
 
 class ImagesList extends Component {
-  state = {};
+  state = {
+    images: [],
+  };
+
+  async componentDidMount() {
+    const { data: images } = await http.get(
+      "https://api.unsplash.com/photos/?client_id=rikz8-JlCCNwLuK8DVuhf6dszY0dWXWop7Xm9TAxMlY"
+    );
+    this.setState({ images });
+  }
+
   render() {
-    return <div>imagenesss</div>;
+    return (
+      <div>
+        imagenesss
+        {this.state.images.map((image) => (
+          <img key={image.id} src={image.urls.raw} />
+        ))}
+      </div>
+    );
   }
 }
 
