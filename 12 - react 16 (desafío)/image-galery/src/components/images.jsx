a32dd19e5a653e5c7ff51a8ef259ccf7185c9845
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-13 16:01:16 -0300

    agrega filtro para las imagenes

--- 12 - react 16 (desafío)/image-galery/src/components/images.jsx
+++ 12 - react 16 (desafío)/image-galery/src/components/images.jsx
@@ -1,27 +1,36 @@
 import { Component } from "react";
 import unsplash from "../services/apiService";
-import Pagination from "./common/pagination";
+// import Pagination from "./common/pagination";
+import SearchBox from "./common/searchBox";
+import Modal from "./modal";
 
 class ImagesList extends Component {
   state = {
     images: [],
     currentPage: 1,
+    searchQuery: "",
+    displayModal: "none",
   };
 
   async componentDidMount() {
-    const { currentPage: page } = this.state;
+    const { currentPage } = this.state;
 
+    await this.loadImage(currentPage);
+  }
+
+  loadImage = async (page) => {
     const { response } = await unsplash.photos.list({
       page,
       perPage: 10,
     });
 
     this.setState({ images: response.results });
-  }
+  };
 
-  loadImage = async (page) => {
-    const { response } = await unsplash.photos.list({
-      page,
+  loadCategory = async (query) => {
+    const { response } = await unsplash.search.getPhotos({
+      query,
+      page: 1,
       perPage: 10,
     });
 
@@ -34,16 +43,44 @@ class ImagesList extends Component {
     this.setState({ currentPage: page });
   };
 
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, currentPage: 1 });
+    this.loadCategory(query);
+  };
+
+  handleImageClick = () => {
+    this.setState({ displayModal: "block" });
+  };
+
+  handleModal = () => {
+    this.setState({ displayModal: "none" });
+  };
+
   render() {
     return (
       <div className="container">
+        <SearchBox
+          value={this.state.searchQuery}
+          onChange={this.handleSearch}
+        />
         {this.state.images.map((image) => (
-          <img key={image.id} src={image.urls.thumb} className="img-fluid" />
+          <img
+            key={image.id}
+            src={image.urls.thumb}
+            className="img-fluid"
+            onClick={this.handleImageClick}
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          />
         ))}
-        <Pagination
+        <Modal
+          displayModal={this.state.displayModal}
+          onOpenModal={this.handleModal}
+        />
+        {/* <Pagination
           currentPage={this.state.currentPage}
           onPageChange={this.handlePageChange}
-        />
+        /> */}
       </div>
     );
   }
---
+++ 12 - react 16 (desafío)/image-galery/src/components/modal.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+class Modal extends Component {
+  render() {
+    return (
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal Title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
