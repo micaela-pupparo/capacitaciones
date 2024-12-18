a061bd947eade85d49afd1f7e8a671647f8b87b5
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-18 15:07:06 -0300

    despacha funciones con redux toolkit

--- 13 - redux/redux-starter/src/store/configureStore.js
+++ 13 - redux/redux-starter/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
@@ -6,7 +6,7 @@ import func from "./middleware/func";
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger("console"), func],
+    middleware: [...getDefaultMiddleware(), logger("console")],
   }); // con toolkit ahora no hace falta especificar que queremos que se hablen con redux devtoools
   // tambien nos deja despachar acciones asincronas
 }
