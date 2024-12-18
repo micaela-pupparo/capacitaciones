1a4cb702a3b1e59e87233903572d5925d90f7fc1
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-18 14:37:38 -0300

    crea un middleware para loggear

--- 13 - redux/redux-starter/src/store/configureStore.js
+++ 13 - redux/redux-starter/src/store/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
+import logger from "./middleware/logger";
 
 export default function () {
   return configureStore({
     reducer,
+    middleware: [logger],
   }); // con toolkit ahora no hace falta especificar que queremos que se hablen con redux devtoools
   // tambien nos deja despachar acciones asincronas
 }
---
+++ 13 - redux/redux-starter/src/store/middleware/logger.js
@@ -0,0 +1,7 @@
+const logger = (store) => (next) => (action) => {
+  console.log("store", store); //es un objeto que PARECE el store. pero tiene solo las propiedades/metodos getState y dispatch
+  console.log("next", next);
+  console.log("action", action);
+};
+
+export default logger;
