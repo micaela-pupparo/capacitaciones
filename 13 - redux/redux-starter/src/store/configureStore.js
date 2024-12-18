f0c2ec84ca5f0be4f1c354772849ef2e49e418e6
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-18 15:14:12 -0300

    crea middleware para mostrar un error

--- 13 - redux/redux-starter/src/store/configureStore.js
+++ 13 - redux/redux-starter/src/store/configureStore.js
@@ -1,12 +1,17 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
+import toast from "./middleware/toast";
+// import func from "./middleware/func";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [...getDefaultMiddleware(), logger("console")],
+    middleware: [
+      ...getDefaultMiddleware(), //middleware Thunk, como Func
+      logger("console"),
+      toast,
+    ],
   }); // con toolkit ahora no hace falta especificar que queremos que se hablen con redux devtoools
   // tambien nos deja despachar acciones asincronas
 }
