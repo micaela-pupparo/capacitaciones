e23d4ec7b5973e06c6d923a2dcb469636e8d2038
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-18 11:43:41 -0300

    crea un slice que combina create actions con create reducer

--- 13 - redux/redux-starter/src/store/bugs.js
+++ 13 - redux/redux-starter/src/store/bugs.js
@@ -7,14 +7,14 @@ const BUG_RESOLVED = "bugResolved";
 
 // Action creators
 // con ducks pattern se debe exportar cada action creator
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
 // const bugUpdated = createAction("bugUpdated");
 // console.log(bugUpdated({ id: 1 })); //este objeto entra en payload
 
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
+// export const bugAdded = createAction("bugAdded");
+// export const bugResolved = createAction("bugResolved");
+// export const bugRemoved = createAction("bugRemoved");
 
 // Reducer
 // con ducks pattern siempre tu reducer tiene que estar exportado en default
@@ -22,19 +22,19 @@ let lastId = 0;
 
 // redux toolkit usa immer para que los objetos sean inmutables
 // bugs seria state
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-});
+// export default createReducer([], {
+//   [bugAdded.type]: (bugs, action) => {
+//     bugs.push({
+//       id: ++lastId,
+//       description: action.payload.description,
+//       resolved: false,
+//     });
+//   },
+//   [bugResolved.type]: (bugs, action) => {
+//     const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+//     bugs[index].resolved = true;
+//   },
+// });
 
 // export default function reducer(state = [], action) {
 //   switch (action.type) {
@@ -60,3 +60,25 @@ export default createReducer([], {
 //       return state;
 //   }
 // }
+
+// slice combina create action y create reducer
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+  },
+});
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
