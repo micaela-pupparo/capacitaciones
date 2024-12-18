37f7ecd529fe8f52a56bbf0ebd803e2489ad719d
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-18 09:34:36 -0300

    crea metodos dispatch y subscribe

--- 13 - redux/redux-starter/src/index.js
+++ 13 - redux/redux-starter/src/index.js
@@ -1,87 +1,26 @@
-console.log("Hello World!");
+import store from "./store";
 
-// Composition
-// sin functional programming
-import { compose, pipe } from "lodash/fp";
+// la funcion es llamada siempre que el esatdo cambia
+// los componentes UI deberian estar suscriptos. pero cuando no estan visibles
+// deberian desuscribirse.
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
 
-let input = "  JavaScript  ";
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
 
-// con functional programming
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
+unsubscribe();
 
-// higher-order function
-// retorna una nueva funcion que es la composicion de todas las funciones que se pasan por parametro
-// el orden es de derecha a izquierda para saber el orden en el que se aplican
-const transform = compose(wrapInDiv, toLowerCase, trim);
-transform(input);
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
 
-// para evitar eso, se usa pipe
-// ahora se lee de izquierda a derecha
-pipe(trim, toLowerCase, wrap("div"));
-
-// Currying
-function add(a) {
-  return function (b) {
-    return a + b;
-  };
-}
-
-const add2 = (a) => (b) => a + b;
-
-add(1)(5);
-
-// Pure Functions
-// funcion NO pura
-function myFunction(number) {
-  return number * Math.random();
-}
-// funcion pura
-function myFunction2(number) {
-  return number * 2;
-}
-
-// funcion NO pura
-const minAge = 18;
-// si cambiamos la variable global, esta funcion va a retornar algo distinto
-function isEligible(age) {
-  return age > minAge;
-}
-
-// funcion pura
-// todo lo que la funcion necesita se debe especificar en la lista de parametros
-function isEligible2(age, minAge1) {
-  return age > minAge1;
-}
-
-// Immutability
-const person = {
-  name: "john",
-  address: { country: "USA", city: "San Francisco" },
-};
-// la propiedad name se sobreescribe
-const updated1 = {
-  ...person,
-  address: { ...person.address, city: "New York" },
-  name: "bob",
-};
-
-console.log(person); // city: "San Francisco"
-
-const numbers = [1, 2, 3];
-
-// Adding
-const index = numbers.indexOf(2);
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)]; // utilizamos el spread operator porque slice devuelve un array y no queremos un array de arrays
-
-// Removing
-const removed = numbers.filter((n) => n !== 2);
-
-// Updating
-const updated = numbers.map((n) => (n === 2 ? 20 : n));
-
-// Immutable
-import { Map } from "immutable";
-
-let book = Map({ title: "Harry Potter" });
+console.log(store.getState());
