4a107e4beb0ecf9e904d0b5df78f343975b8a7a5
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-19 13:19:44 -0300

    limpia el codigo en los tests

--- 13 - redux/redux-starter/src/store/tests/bugs.spec.js
+++ 13 - redux/redux-starter/src/store/tests/bugs.spec.js
@@ -5,6 +5,15 @@ import configureStore from "../configureStore";
 
 describe("bugsSlice", () => {
   describe("action creators", () => {
+    let fakeAxios;
+    let store;
+
+    beforeEach(() => {
+      fakeAxios = new MockAdapter(axios);
+      store = configureStore();
+    });
+
+    const bugsSlice = () => store.getState().entities.bugs;
     // el problema aca es que en configureStore, en middleware tenemos la api. si lo comentamos, este test sigue pasando.
     // tambien si queremos testear lo que pasamos en middleware los test ya sabrian demasiado sobre la implementacion
     // it("addBug", () => {
@@ -24,19 +33,34 @@ describe("bugsSlice", () => {
     // });
 
     // tira error porque se comunica con el backend. deja de ser una unit test y pasa a una integration test
-    it("should handle the addBug action", async () => {
+    it("should add the bug to the store if its saved to the server", async () => {
       // dispatch(addBug) => store
       //   ahora tenemos un store con todos los middlewares. no nos importan porque estamos testeando el comportamiento, no la implementacion
+      //   Arrange
       const bug = { description: "a" };
       const savedBug = { ...bug, id: 1 };
-
-      const fakeAxios = new MockAdapter(axios);
       fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-      const store = configureStore();
+      //   Act
       await store.dispatch(addBug(bug));
 
-      expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+      //   Assert
+      expect(bugsSlice().list).toContainEqual(savedBug);
+    });
+
+    it("should not add the bug to the store if its not saved to the server", async () => {
+      // dispatch(addBug) => store
+      //   ahora tenemos un store con todos los middlewares. no nos importan porque estamos testeando el comportamiento, no la implementacion
+      //   Arrange
+      const bug = { description: "a" };
+      const savedBug = { ...bug, id: 1 };
+      fakeAxios.onPost("/bugs").reply(500);
+
+      //   Act
+      await store.dispatch(addBug(bug));
+
+      //   Assert
+      expect(bugsSlice().list).toHaveLength(0);
     });
   });
 });
