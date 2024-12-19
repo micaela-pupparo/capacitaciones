1583b2be5547ac3d3453c4fbd9e40ba1f6c51bb2
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2024-12-19 10:19:19 -0300

    impide llamar otra vez al backend si pasaron menos de 10 minutos

--- 13 - redux/redux-starter/src/store/bugs.js
+++ 13 - redux/redux-starter/src/store/bugs.js
@@ -10,6 +10,7 @@ const BUG_RESOLVED = "bugResolved";
 import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "@reduxjs/toolkit";
 import { apiCallBegan } from "./api";
+import moment from "moment";
 
 // const bugUpdated = createAction("bugUpdated");
 // console.log(bugUpdated({ id: 1 })); //este objeto entra en payload
@@ -69,7 +70,7 @@ const slice = createSlice({
   initialState: {
     list: [],
     loading: false,
-    lastFeth: null,
+    lastFetch: null,
   },
   reducers: {
     // creando un reducer automaticamente nos crea una accion
@@ -79,6 +80,7 @@ const slice = createSlice({
     bugsReceived: (bugs, action) => {
       bugs.list = action.payload;
       bugs.loading = false;
+      bugs.lastFetch = Date.now();
     },
     bugsRequestFailed: (bugs, action) => {
       bugs.loading = false;
@@ -115,15 +117,24 @@ export default slice.reducer;
 // Action Creators
 const url = "/bugs"; //esto podria estar en un config file
 
-export const loadBugs = () =>
-  apiCallBegan({
-    url,
-    // method: "get", por default el metodo es get
-    // data: {},
-    onStart: bugsRequested.type,
-    onSuccess: bugsReceived.type,
-    onError: bugsRequestFailed.type,
-  });
+export const loadBugs = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.bugs;
+
+  const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
+  // el 10 deberia estar tambien en un config file, ademas esta implementacion deberia ser generalizada
+  if (diffInMinutes < 10) return;
+
+  dispatch(
+    apiCallBegan({
+      url,
+      // method: "get", por default el metodo es get
+      // data: {},
+      onStart: bugsRequested.type,
+      onSuccess: bugsReceived.type,
+      onError: bugsRequestFailed.type,
+    })
+  );
+};
 
 // Selector Function, funcion que toma el estado y retorna el estado computado
 getUnresolvedBugsMalHecho = (state) =>
