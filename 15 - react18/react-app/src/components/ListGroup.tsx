f5710c4636267f6cbda243c5bcf2ecbac6c79d0c
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-02 10:55:36 -0300

    maneja eventos

--- 15 - react18/react-app/src/components/ListGroup.tsx
+++ 15 - react18/react-app/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
@@ -6,6 +8,8 @@ function ListGroup() {
   //     return items.length === 0 ? <p>No item found</p> : null;
   //   };
   //   lo que tiene de bueno las funciones es que se le pueden pasar parametros y renderiza un mensaje distinto
+
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
@@ -16,8 +20,12 @@ function ListGroup() {
         {items.length === 0 ? <p>No item found</p> : null}
       */}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(e) => console.log(item, index, e)}
+          >
             {item}
           </li>
         ))}
