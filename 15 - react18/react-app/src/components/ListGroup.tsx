97fdabb574f3d7786f28ef648694f2ba95c2cf62
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-02 10:46:51 -0300

    agrega formas de renderizado condicional

--- 15 - react18/react-app/src/components/ListGroup.tsx
+++ 15 - react18/react-app/src/components/ListGroup.tsx
@@ -1,9 +1,20 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
+  //   const message = items.length === 0 ? <p>No item found</p> : null;
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null;
+  //   };
+  //   lo que tiene de bueno las funciones es que se le pueden pasar parametros y renderiza un mensaje distinto
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      {/* otras formas
+        {message}
+        {getMessage()}
+        {items.length === 0 ? <p>No item found</p> : null}
+      */}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
