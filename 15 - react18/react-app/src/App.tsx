f88f88828b54577ef0e37ce3b79e74af2c79dca9
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-14 11:55:36 -0300

    crea componente lista de expensas

--- 15 - react18/react-app/src/App.tsx
+++ 15 - react18/react-app/src/App.tsx
@@ -85,12 +85,34 @@ import ListGroup from "./components/ListGroup";
 //     </div>
 //   );
 // }
-import Form from "./components/Form";
+
+// import Form from "./components/Form";
+
+// function App() {
+//   return (
+//     <div>
+//       <Form></Form>
+//     </div>
+//   );
+// }
+
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
