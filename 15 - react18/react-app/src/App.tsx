209b8205dead7688b795cf12df2827decd5eaa69
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-14 13:30:46 -0300

    agrega el formulario para agregar expensas

--- 15 - react18/react-app/src/App.tsx
+++ 15 - react18/react-app/src/App.tsx
@@ -97,7 +97,9 @@ import ListGroup from "./components/ListGroup";
 // }
 
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import categories from "./expense-tracker/categories";
 import { useState } from "react";
 
 function App() {
@@ -115,6 +117,13 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
