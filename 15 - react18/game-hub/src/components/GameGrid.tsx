6ac2302ef5d9b1c20778c418816a77fd54af622e
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-15 15:12:56 -0300

    crea un hook custom para obtener los juegos mediante fetch

--- 15 - react18/game-hub/src/components/GameGrid.tsx
+++ 15 - react18/game-hub/src/components/GameGrid.tsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    // necesitamos definir el type de la response. para eso vemos la respuesta que te da la api en la documentacion
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
