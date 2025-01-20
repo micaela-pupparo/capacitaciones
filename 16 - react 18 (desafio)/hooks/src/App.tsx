f257f897416c63b960593ec5f195780a98079feb
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-20 11:19:13 -0300

    crea un ejemplo para un mal uso de useActionState

--- 16 - react 18 (desafio)/hooks/src/App.tsx
+++ 16 - react 18 (desafio)/hooks/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./useActionState/buen uso/form";
+import WrongForm from "./useActionState/mal uso/WrongForm";
 
 function App() {
   return (
     <>
-      <Form />
+      {/* <Form /> */}
+      <WrongForm />
     </>
   );
 }
---
+++ 16 - react 18 (desafio)/hooks/src/useActionState/mal uso/WrongForm.tsx
@@ -0,0 +1,150 @@
+import { useActionState } from "react";
+
+async function genericAction(previousState: object, formData: FormData) {
+  // solo actualiza una cosa por mas de que en el form 2 tenga name y username
+  if (formData.has("username")) {
+    const newUsername = formData.get("username");
+
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1",
+      {
+        method: "PATCH",
+        body: JSON.stringify({ username: newUsername }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    if (!response.ok) {
+      const { message } = await response.json();
+      return { message, previousState };
+    }
+
+    return { username: newUsername };
+  }
+
+  if (formData.has("name")) {
+    const newName = formData.get("name");
+
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments/1",
+      {
+        method: "PATCH",
+        body: JSON.stringify({ name: newName }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    if (!response.ok) {
+      const { message } = await response.json();
+      return { message, previousState };
+    }
+
+    return { name: newName };
+  }
+}
+
+const WrongForm = () => {
+  const [state, submitAction, isPending] = useActionState(genericAction, "");
+  const name = state.name ? state.name : "";
+  const username = state.username ? state.username : "";
+
+  console.log(state);
+
+  return (
+    <>
+      <div>
+        <div>Formulario 1</div>
+        <form action={submitAction}>
+          <div style={{ marginBottom: 30 }}>
+            <label
+              htmlFor="name"
+              style={{
+                display: "block",
+                textAlign: "left",
+                fontSize: 14,
+              }}
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nombre..."
+              defaultValue={name}
+              style={{ padding: 6, marginTop: 6 }}
+            />
+            {name && !isPending && (
+              <p style={{ fontSize: 8, textAlign: "left", margin: 0 }}>
+                Nombre del comentario actualizado a: {name}
+              </p>
+            )}
+          </div>
+          <button disabled={isPending} type="submit">
+            {isPending ? "Actualizando..." : "Actualizar"}
+          </button>
+        </form>
+      </div>
+      <div style={{ marginTop: 40 }}>
+        <div>Formulario 2</div>
+        <form action={submitAction}>
+          <div style={{ marginBottom: 30 }}>
+            <label
+              htmlFor="name"
+              style={{
+                display: "block",
+                textAlign: "left",
+                fontSize: 14,
+              }}
+            >
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Nombre..."
+              defaultValue={name}
+              style={{ padding: 6, marginTop: 6 }}
+            />
+            {name && !isPending && (
+              <p style={{ fontSize: 8, textAlign: "left", margin: 0 }}>
+                Nombre del usuario actualizado a: {name}
+              </p>
+            )}
+          </div>
+          <div style={{ marginBottom: 30 }}>
+            <label
+              htmlFor="username"
+              style={{
+                display: "block",
+                textAlign: "left",
+                fontSize: 14,
+              }}
+            >
+              Usuario
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Usuario..."
+              defaultValue={username}
+              style={{ padding: 6, marginTop: 6 }}
+            />
+            {username && !isPending && (
+              <p style={{ fontSize: 8, textAlign: "left", margin: 0 }}>
+                Usuario actualizado a: {username}
+              </p>
+            )}
+          </div>
+          <button disabled={isPending} type="submit">
+            {isPending ? "Actualizando..." : "Actualizar"}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default WrongForm;
