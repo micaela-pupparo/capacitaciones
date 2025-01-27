1ffbf871f10eba3be3d220fda7e9c478f98fd059
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-27 15:31:49 -0300

    fetching generos

--- 17 - react18 (intermediate)/react-course-part2-starter-main/src/main.tsx
+++ 17 - react18 (intermediate)/react-course-part2-starter-main/src/main.tsx
@@ -6,7 +6,18 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000,
+      staleTime: 0,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
