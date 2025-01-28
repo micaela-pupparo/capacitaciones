389b4ab94129c2c840ce04ceaf93a9bb0de0d830
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-01-28 09:54:53 -0300

    refactoriza el fetch de games con react query

--- 17 - react18 (intermediate)/game-hub-main/src/hooks/useGames.ts
+++ 17 - react18 (intermediate)/game-hub-main/src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import { FetchResponse } from "../services/api-client";
+import apiClient from "../services/api-client";
 
 export interface Platform {
   id: number;
@@ -18,17 +19,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
+  // siempre que los valores cambien en el objeto que le pasamos a useQuery, react query va a voler a fetchear los juegos del backend
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    // el segundo parametro del get es un objeto de configuracion para pasar nuestro query string parameters al backend
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
