a482a1cca606c863831a516246d432c0ec1ad4e6
Micaela Pupparo <micaela.pupparo@moxit.com.ar>
2025-02-14 14:26:00 -0300

    setea mock service worker

--- 19- react 18 testing/react-testing-starter-main/tests/setup.ts
+++ 19- react 18 testing/react-testing-starter-main/tests/setup.ts
@@ -1,5 +1,13 @@
 import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
+import {server} from "./mocks/server";
+
+// antes de los archivo de prueba, empezamos el servidor para que escuche asi escucha los pedidos a los endpoints configurados
+beforeAll(() => server.listen());
+// debemos resetear nuestros handlers despues de que un archivo de prueba se ejecute para que cada archivo empiece con un estado limpio
+afterEach(() => server.resetHandlers());
+// luego de todos los tests, queremos cerrar el servidor
+afterAll(() => server.close())
 
 global.ResizeObserver = ResizeObserver;
 
